c800e4342488308902ef0c4d4665518886404f25
Adam Esterline <adam@esterlines.com>
2013-01-24 21:55:14 -0700

    Testing with clojure 1.5-RC1 and jdk 7

--- project.clj
+++ project.clj
@@ -4,6 +4,6 @@
   :license {:name "Eclipse Public License 1.0"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[io.netty/netty "3.6.2.Final"]]
-  :profiles {:dev {:dependencies [[org.clojure/clojure "1.4.0"]
-                                  [clj-http "0.5.2"]
-                                  [compojure "1.1.1"]]}})
+  :profiles {:shared {:dependencies [[clj-http "0.5.2"] [compojure "1.1.1"]]}
+             :dev [:shared {:dependencies [[org.clojure/clojure "1.4.0"]]}]
+             :1.5 [:shared {:dependencies [[org.clojure/clojure "1.5.0-RC1"]]}]})
