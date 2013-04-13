75ba03d01807b62b7d074ae0a2feb74fe566d79a
Adam Esterline <adam@esterlines.com>
2013-04-12 20:37:27 -0600

    Updated to Netty 3.6.5.Final

--- project.clj
+++ project.clj
@@ -3,9 +3,9 @@
   :url "http://github.com/RallySoftware/netty-ring-adapter"
   :license {:name "Eclipse Public License 1.0"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[io.netty/netty "3.6.3.Final"]
+  :dependencies [[io.netty/netty "3.6.5.Final"]
                  [org.clojure/tools.logging "0.2.6"]]
-  :profiles {:shared {:dependencies [[clj-http "0.6.5"] [compojure "1.1.5"]]}
+  :profiles {:shared {:dependencies [[clj-http "0.7.1"] [compojure "1.1.5"]]}
              :dev [:shared {:dependencies [[org.clojure/clojure "1.4.0"]]}]
              :1.5 [:shared {:dependencies [[org.clojure/clojure "1.5.1"]]}]}
   :jvm-opts ["-Djava.util.logging.config.file=logging.properties"])
